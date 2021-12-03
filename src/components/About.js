47d83d3c6f9cd356e3995e24a328819c7bba0ffb
Ayush Yadav <ayushyadav935@gmail.com>
2021-12-03 12:17:43 +0530

    About section

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import React from "react";
 
 const About = () => {
     return (
-        <div>
+        <div style={{width:"60%", margin:"0 auto"}}>
             <h1>i-Notes</h1>
-            <h5 className="mt-4">   
+            <h5 className="mt-4" style={{fontWeight:"300"}}>   
                 i-Notes is a free-form notes web application.
                 It allows you to store all of your notes and information in an easy-to-use outline, where 
                 you can quickly find what you need. Here users can also update their notes and delet and created a new one.
