8ba16233ce5c6f94088217f0aba9813adfaab7e2
Ayush Yadav <ayushyadav935@gmail.com>
2021-12-16 19:53:54 +0530

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,7 @@ const About = () => {
             <h5 className="mt-4" style={{fontWeight:"300"}}>   
                 i-Notes is a free-form notes web application.
                 It allows you to store all of your notes and information in an easy-to-use outline, where 
-                you can quickly find what you need. Here users can also update their notes and delet and created a new one.
+                you can quickly find what you need. Here users can update their notes and delete and created a new one.
                 Creating your own freeform database and getting organized couldn't be easier...
             </h5>
         </div>
